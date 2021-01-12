c1376bd5e1743b93f93ac2a2b6371bd50006f5fa
ua7605 <vincent.charpentier@student.uantwerpen.be>
2021-01-12 11:08:47 +0100

    added JSON reader
    This makes it possible to read in a dustConfiguration file. Such that the TB can sub on the sub channels.

--- src/main/java/JSON.java
+++ src/main/java/JSON.java
@@ -1,7 +1,47 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Discription:
  */
 public class JSON {
+
+
+
+    public JSON() throws IOException, ParseException {
+
+        JSONParser jsonParser = new JSONParser();
+
+        FileReader reader = new FileReader("DUSTconfig.json");
+
+        Object object = jsonParser.parse(reader);
+
+        JSONObject jsonObject = (JSONObject) object;
+
+        String blockname = (String) jsonObject.get("BlockName-of-subscribe-block");
+
+
+        JSONArray channelarray = (JSONArray) jsonObject.get("list-of-channels");
+
+        for (int i= 0; i< channelarray.size();i++){
+            JSONObject channelNameobj = (JSONObject) channelarray.get(i);
+
+            String channelName = (String)channelNameobj.get("channelName"+i);
+            System.out.println(channelName);
+        }
+
+
+
+
+
+    }
+
 
 
 
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,12 @@
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+
 public class main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException {
 
-
+        JSON json = new JSON();
 
 
 
